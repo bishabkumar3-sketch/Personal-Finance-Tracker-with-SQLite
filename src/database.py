daae0842114ce2c25ec1a9f9ddd8adc05041ebb0
bishabkumar <bishabkumar3@.gmail>
2026-07-13 14:53:38 +0530

    add database.py file

---
+++ src/database.py
@@ -0,0 +1,17 @@
+import sqlite3
+db_name = "finance_tracker.db"
+
+def create_connection():
+    return sqlite3.connect(db_name)
+ 
+def view_transactions():
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM transactions")
+
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return rows
