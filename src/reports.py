c22f5677bbfd14f7775ff51ccebfdc2d3f7dccd1
bishabkumar3@gmai.com <bishabkumar3@gmail.com>
2026-08-03 01:18:12 +0530

    intial commit

--- src/reports.py
+++ src/reports.py
@@ -12,3 +12,23 @@ def load_data():
     return df
 
 
+def expense_by_category():
+    
+    df = load_data()
+    expenses = df[df["type"] == "expense"]
+    category_expenses = expenses.groupby("category")["amount"].sum()
+
+    plt.figure(figsize=(10, 6))
+
+    category_expenses.plot(kind = "bar", color = "skyblue")
+
+    plt.title("Expenses by Category")
+    plt.xlabel("Category")
+    plt.ylabel("AMOUNT")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    plt.savefig("reports/expense_by_category.png")
+
+    plt.close()
+    plt.show()
