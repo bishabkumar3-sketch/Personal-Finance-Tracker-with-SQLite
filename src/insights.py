659fb34512985a5fc56329f09450584a3c362d3f
bishabkumar3@gmai.com <bishabkumar3@gmail.com>
2026-08-18 21:19:08 +0530

    intial commit

--- src/insights.py
+++ src/insights.py
@@ -2,4 +2,10 @@ import sqlite3
 import pandas as pd 
 
 def load_data():
-    
+    conn = sqlite3.connect('finance.db')
+
+    df = pd.read_sql_query("SELECT * FROM transactions", conn)
+
+    conn.close()
+    return df
+
